ecbdc6ab9233faf3cb7d3b91f06703e3339d36f6
unknown <a.cina@procreatech.com>
2024-12-20 13:40:25 +0100

    85. Angular Standalone Components - New Section

---
+++ src/app/course-title/course-title.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'course-title',
+  imports: [],
+  templateUrl: './course-title.component.html',
+  styleUrl: './course-title.component.css'
+})
+export class CourseTitleComponent {
+
+}
